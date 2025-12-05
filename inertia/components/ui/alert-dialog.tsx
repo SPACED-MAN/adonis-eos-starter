eb5030954e62b6094d0bf69b00b2953409c129a2
SPACED-MAN <tyler@modernaut.com>
2025-12-05 05:29:09 +0000

    Adjust alert dialog margins

--- inertia/components/ui/alert-dialog.tsx
+++ inertia/components/ui/alert-dialog.tsx
@@ -46,7 +46,7 @@ export const AlertDialogHeader = ({ className, ...props }: React.HTMLAttributes<
 )
 
 export const AlertDialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-	<div className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)} {...props} />
+	<div className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 mt-6', className)} {...props} />
 )
 
 export const AlertDialogTitle = React.forwardRef<
